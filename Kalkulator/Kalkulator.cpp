2510e63995f5b973e1939eacf526a43a9ec26039
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2022-10-17 20:50:51 +0200

    first commit

---
+++ Kalkulator/Kalkulator.cpp
@@ -0,0 +1,25 @@
+﻿// Kalkulator.cpp: Definuje vstupní bod pro aplikaci.
+//Author: Michal Basler (247094)
+/*
+16) Statistická kalkulačka (průměr, rozptyl, histogramy)
+Úkolem bude vytvoření aplikace pro statistické výpočty. Jako vstupní parametry budou sloužit data
+získaná z textového souboru či vstup z konzole.
+Základní parametry programu:
+1. Aplikace bude rozdělena do dvou statistických sekcí. První možností bude zpracování
+číselných hodnot, nad kterými budou prováděny statistické operace (průměr, vážený průměr,
+rozptyl, směrodatná odchylka, histogram, medián).
+2. Druhou částí budou statistické výpočty nad textem. Program bude umožňovat výpočet počtu
+znaků, počtu slov, histogram písmen a délek slov.
+3. Výsledky bude možné uložit do textového souboru.
+Bonusový úkol: Implementujte možnost vykreslení histogramu a gaussova rozložení do konzole
+*/
+
+#include "Kalkulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
